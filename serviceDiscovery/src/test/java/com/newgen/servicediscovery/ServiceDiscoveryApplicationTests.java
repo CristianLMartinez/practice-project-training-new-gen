2b7a92ecfd3337469dbeb959b758cecb428394bf
CristianLMartinez <cristian.lm@globant.com>
2024-07-25 16:35:35 -0500

    configure my first discovery server and discovery client

---
+++ serviceDiscovery/src/test/java/com/newgen/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newgen.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
